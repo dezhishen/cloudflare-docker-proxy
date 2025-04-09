c029476d337e3bf3f941207dfe30819e68874e3b
shendezhi <shendz@cecinvestment.com>
2025-04-09 09:36:09 +0800

    mod:修改域名

--- src/index.js
+++ src/index.js
@@ -8,16 +8,16 @@ const dockerHub = "https://registry-1.docker.io";
 const routes = {
   // production
   ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  ["quay-docker." + CUSTOM_DOMAIN]: "https://quay.io",
+  ["gcr-docker." + CUSTOM_DOMAIN]: "https://gcr.io",
+  ["k8s-gcr-docker." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
+  ["k8s-docker." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
+  ["ghcr-docker." + CUSTOM_DOMAIN]: "https://ghcr.io",
+  ["cloudsmith-docker." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
+  ["ecr-docker." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
 
   // staging
-  ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
+  // ["staging-docker." + CUSTOM_DOMAIN]: dockerHub,
 };
 
 function routeByHosts(host) {
@@ -154,7 +154,7 @@ async function fetchToken(wwwAuthenticate, scope, authorization) {
 }
 
 function responseUnauthorized(url) {
-  const headers = new(Headers);
+  const headers = new (Headers);
   if (MODE == "debug") {
     headers.set(
       "Www-Authenticate",
